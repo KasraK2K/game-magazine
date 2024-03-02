956f59e95b2edaaa585436ad86ce0ee1df30ef0d
Kasra Karami <Kasra.Karami.Work@gmail.com>
2024-03-02 17:51:10 +0330

    fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,31 +1,8 @@
-import { useEffect, useState } from 'react'
 import { Text } from '@chakra-ui/react'
-import apiClient from '../services/api-client'
-
-interface Game {
-    id: number
-    name: string
-}
-
-interface FetchGamesResponse {
-    count: number
-    results: Game[]
-}
+import useGames from '../hooks/useGames'
 
 const GameGrid = () => {
-    const [games, setGames] = useState<Game[]>([])
-    const [error, setError] = useState('')
-
-    useEffect(() => {
-        const controller = new AbortController()
-
-        apiClient
-            .get<FetchGamesResponse>('/games', { signal: controller.signal })
-            .then((response) => setGames(response.data.results))
-            .catch((err) => setError(err.message))
-
-        return () => controller.abort()
-    }, [])
+    const { games, error } = useGames()
 
     return (
         <>
