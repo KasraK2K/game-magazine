e566cfcadec6bae60c687f9d91b51705251847a8
Kasra Karami <Kasra.Karami.Work@gmail.com>
2024-03-03 22:05:12 +0330

    Remove unnecessary fragment

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -13,25 +13,23 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
     if (error) return null
     if (isLoading) return <Spinner />
     return (
-        <>
-            <List>
-                {data.map((genre) => (
-                    <ListItem key={genre.id} paddingY='5px'>
-                        <HStack>
-                            <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-                            <Button
-                                fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
-                                onClick={() => onSelectGenre(genre)}
-                                fontSize='large'
-                                variant='link'
-                            >
-                                {genre.name}
-                            </Button>
-                        </HStack>
-                    </ListItem>
-                ))}
-            </List>
-        </>
+        <List>
+            {data.map((genre) => (
+                <ListItem key={genre.id} paddingY='5px'>
+                    <HStack>
+                        <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
+                        <Button
+                            fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
+                            onClick={() => onSelectGenre(genre)}
+                            fontSize='large'
+                            variant='link'
+                        >
+                            {genre.name}
+                        </Button>
+                    </HStack>
+                </ListItem>
+            ))}
+        </List>
     )
 }
 
