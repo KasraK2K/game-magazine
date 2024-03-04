386f83e0cf762bfdec838706834bb3ec3cdb8f20
Kasra Karami <Kasra.Karami.Work@gmail.com>
2024-03-04 22:02:26 +0330

    Create search component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from '@chakra-ui/react'
 import gameMagLogo from '../assets/game_mag.webp'
 import ColorModeSwitch from './ColorModeSwitch'
+import SearchInput from './SearchInput'
 
 const NavBar = () => {
     return (
-        <HStack justifyContent='space-between' padding='10px'>
+        <HStack padding='10px'>
             <Image src={gameMagLogo} boxSize='60px' />
+            <SearchInput />
             <ColorModeSwitch />
         </HStack>
     )
