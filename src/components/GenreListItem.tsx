72c948364e4d41dd557e7a6dd33403887d03c89e
Kasra Karami <Kasra_K2K@yahoo.com>
2024-03-29 18:45:46 +0330

    BugFix set Zustand game query

---
+++ src/components/GenreListItem.tsx
@@ -0,0 +1,34 @@
+import { Button, HStack, Image, ListItem } from '@chakra-ui/react'
+import { Genre } from '../hooks/useGenres'
+import useGameQueryStore from '../stores/gameStore'
+
+interface Props {
+    genre: Genre
+}
+
+const GenreListItem = ({ genre }: Props) => {
+    const selectedGenreId = useGameQueryStore((s) => s.gameQuery.genreId)
+    const setSelectedGenreId = useGameQueryStore((s) => s.setGenreId)
+
+    return (
+        <>
+            <ListItem paddingY='5px'>
+                <HStack>
+                    <Image boxSize='32px' borderRadius={8} src={genre.image_background} objectFit='cover' />
+                    <Button
+                        whiteSpace='normal'
+                        textAlign='left'
+                        fontWeight={genre.id === selectedGenreId ? 'bold' : 'normal'}
+                        onClick={() => setSelectedGenreId(genre.id)}
+                        fontSize='large'
+                        variant='link'
+                    >
+                        {genre.name}
+                    </Button>
+                </HStack>
+            </ListItem>
+        </>
+    )
+}
+
+export default GenreListItem
--- src/stores/gameStore.ts
+++ src/stores/gameStore.ts
@@ -19,9 +19,9 @@ interface GameQueryStore {
 const useGameQueryStore = create<GameQueryStore>((set) => ({
     gameQuery: {},
     setSearchText: (searchText) => set(() => ({ gameQuery: { searchText } })),
-    setGenreId: (genreId) => set((store) => ({ gameQuery: { ...store, genreId } })),
-    setPlatformId: (platformId) => set((store) => ({ gameQuery: { ...store, platformId } })),
-    setSortOrder: (sortOrder) => set((store) => ({ gameQuery: { ...store, sortOrder } })),
+    setGenreId: (genreId) => set((store) => ({ gameQuery: { ...store.gameQuery, genreId } })),
+    setPlatformId: (platformId) => set((store) => ({ gameQuery: { ...store.gameQuery, platformId } })),
+    setSortOrder: (sortOrder) => set((store) => ({ gameQuery: { ...store.gameQuery, sortOrder } })),
 }))
 
 export default useGameQueryStore
