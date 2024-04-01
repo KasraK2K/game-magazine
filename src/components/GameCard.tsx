88c406bb3e19006f9fe119b7a7e9b726ca954875
Kasra Karami <Kasra_K2K@yahoo.com>
2024-04-01 10:45:50 +0330

    Create game details page

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,5 @@
 import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 import { Game } from '../hooks/useGames'
 import getCroppedImageUrl from '../services/image-url'
 import CriticScore from './CriticScore'
@@ -10,14 +11,21 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
     return (
-        <Card borderRadius={10} height='100%' overflow='hidden'>
+        <Card
+            borderRadius={10}
+            height='100%'
+            overflow='hidden'
+            _hover={{ transform: 'scale(1.03)', transition: 'transform 0.15s ease-in' }}
+        >
             <Image src={getCroppedImageUrl(game.background_image)} />
             <CardBody>
                 <HStack justifyContent='space-between' alignItems='start' marginBottom={3}>
                     <PlatformIconList platform={game.parent_platforms?.map((p) => p.platform)} />
                     <CriticScore score={game.metacritic} />
                 </HStack>
-                <Heading fontSize='2xl'>{game.name}</Heading>
+                <Heading fontSize='2xl'>
+                    <Link to={`/games/${game.slug}`}>{game.name}</Link>
+                </Heading>
             </CardBody>
         </Card>
     )
